3aa1a8e3d7a96618cda280e03da2154839d4996e
ethan_e <eedmond@uccs.edu>
2020-10-28 12:35:33 -0600

    Refactored whitespace in js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 var makeButton = function (name) {
-    var button = {};
-    button.htmElement = document.getElementById(name);
+  var button = {};
+  button.htmElement = document.getElementById(name);
 
-    var thicken = function () {
-        button.htmElement.style.fontWeight = 700;
-    };
-    button.htmElement.addEventListener("mouseenter",thicken);
+  var thicken = function () {
+    button.htmElement.style.fontWeight = 700;
+  };
+  button.htmElement.addEventListener("mouseenter",thicken);
 
-    var thinner = function () {
-        button.htmElement.style.fontWeight = 100;
-    };
-    button.htmElement.addEventListener("mouseleave", thinner);
+  var thinner = function () {
+    button.htmElement.style.fontWeight = 100;
+  };
+  button.htmElement.addEventListener("mouseleave", thinner);
 
-    return button;
+  return button;
 };
 
 var buttonList = ["galleryButton", "aboutButton", "projectButton", "aboutSectButton", "unpluggedButton", "magic8Button", "rogueButton"];
 for (var i = 0; i < buttonList.length; i++) {
-    buttonList[i] = makeButton(buttonList[i]);
+  buttonList[i] = makeButton(buttonList[i]);
 }
